155bd374eff446bbfdf73880e590a598e7ec5a18
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2022-02-05 10:56:40 -0300

    alterações no projeto

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { pokeApi } from "../../services/api";
-import { Wrapper } from "./styles";
+import { Checkbox, Filter, Search, Wrapper } from "./styles";
+
+interface IPokeResult {}
 
 const Pokedex = () => {
   const [pokemons, setPokemons] = useState<Array<any>>([]);
+  const [pokeTypes, setPokeTypes] = useState<Array<any>>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
@@ -16,15 +19,46 @@ const Pokedex = () => {
       .finally(() => setLoading(false));
   }, []);
 
+  useLayoutEffect(() => {
+    pokeApi
+      .get("type")
+      .then((response) => setPokeTypes(response.data.results))
+      .catch()
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <Wrapper>
       {loading && <h1>Loading ...</h1>}
-      {pokemons.map((pokemon: any) => (
+
+      <Filter>
+        <Search>
+          <h3>
+            800 <strong>Pokemons</strong> for you to choose your favorite
+          </h3>
+          <input type="text" placeholder="Encuentra tu pokémon..." />
+        </Search>
+
+        <Checkbox>
+          <select name="" id=""></select>
+
+          <select name="" id="">
+            <option value="">fire</option>
+            <option value="">normal</option>
+          </select>
+          <select name="" id="">
+            <option value="">fire</option>
+            <option value="">normal</option>
+          </select>
+        </Checkbox>
+      </Filter>
+
+      {/* {pokemons.map((pokemon: any) => (
         <div key={pokemon.name}>
           <h1>{pokemon.name} - </h1>
           <h1>{pokemon.url}</h1>
         </div>
-      ))}
+      ))} */}
     </Wrapper>
   );
 };
--- src/pages/Pokedex/styles.ts
+++ src/pages/Pokedex/styles.ts
@@ -1,3 +1,54 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.section``;
+export const Wrapper = styled.section`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 16px 160px;
+`;
+
+export const Filter = styled.section``;
+
+export const Search = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h3 {
+    font-style: normal;
+    font-weight: normal;
+    font-size: var(--h3-size);
+    line-height: 40px;
+
+    letter-spacing: 4px;
+    color: var(--dark-color);
+  }
+
+  input {
+    width: 100%;
+    height: 56px;
+    padding: 0 32px;
+
+    border-radius: 40px;
+    border: none;
+
+    background: var(--light-color);
+    box-shadow: 4px 4px 16px rgba(1, 28, 64, 0.2);
+
+    font-style: normal;
+    font-weight: normal;
+    font-size: var(--paragraph-size);
+    line-height: 20px;
+
+    color: var(--dark-color);
+
+    :focus {
+      border-color: var(--primary-color);
+      box-shadow: 0 1px 1px var(--primary-color), 0 0 8px var(--primary-color);
+      outline: 0 none;
+    }
+  }
+`;
+
+export const Checkbox = styled.article``;
